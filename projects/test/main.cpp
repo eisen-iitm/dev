a71a50b6a13eada56a55936c4918254a78fee727
eisen-iitm <trajeshreddy@gmail.com>
2016-10-09 12:02:47 -0400

    first commit

---
+++ projects/test/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout<<"Enter a number: ";
+    int a;
+    std::cin>>a;
+
+    std::cout<<"Entered number = "<<a<<std::endl;
+
+    return 0;
+}
